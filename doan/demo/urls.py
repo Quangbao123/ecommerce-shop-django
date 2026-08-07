bc9263e66470cbb207e172385718d62e78308976
Bao Nguyen <nguyenquangbao575@gmail.com>
2026-08-07 10:40:08 +0700

    Add new feature add-to-cart and render_cart

--- doan/demo/urls.py
+++ doan/demo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('add-to-cart/<int:id>', views.add_to_cart_view, name='add_product_to_cart'),
+    path('view-cart', views.view_product_cart, name='view_product_cart')
 ]
--- doan/demo/views.py
+++ doan/demo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from product.models import Product
+from django.http import JsonResponse
 import json
 # Create your views here.
 # ----------------- HOME -----------------
@@ -8,3 +9,36 @@ def home_view(request):
     for product in products:
         product.image_filenames = json.loads(product.image)
     return render(request, 'index.html', {'products': products})
+
+# ----------------- ADD PRODUCT TO CART -----------------
+def add_to_cart_view(request, id):
+    if request.method == 'POST':
+        product = Product.objects.get(id=id)
+        product.image_filenames = json.loads(product.image)
+        sale = 1 if product.status == 0 else (100 - product.sale)/100
+        product.final_price = product.price * sale
+
+        cart = request.session.get('cart', {})
+        if str(id) in cart:
+            cart[str(id)]['quantity'] += 1
+        else:
+            cart[str(id)] = {
+                'id': product.id,
+                'name': product.name,
+                'image': product.image_filenames[0],
+                'price': product.final_price,
+                'quantity': 1,
+            }
+        request.session['cart'] = cart
+        request.session.modified = True
+
+        return JsonResponse({
+            'success': True,
+            'status': 'success'
+        })
+
+# ----------------- VIEW PRODUCT CART -----------------
+def view_product_cart(request):
+    return render(request, 'cart.html', {
+        'no_left': True
+    })
